e58148a73fe82728d7e90c3398a581a8bb1ba1d6
Athul <athulea252@gmail.com>
2024-10-04 20:11:18 +0530

    mongodb updated

--- Config/Dbconnection.js
+++ Config/Dbconnection.js
@@ -1,10 +1,31 @@
+// const mongoose = require('mongoose');
+// const connectDb = async () =>{
+//     try {
+//         const connect = await mongoose.connect(process.env.CONNECTION_STRING);
+//         console.log(`database is connected in :${connect.connection.host}`)
+//     } catch (error) {
+//         console.log(error)  
+//     }
+// }
+// module.exports = connectDb;
+
+
+
+
+//
 const mongoose = require('mongoose');
-const connectDb = async () =>{
+
+const connectDb = async () => {
     try {
-        const connect = await mongoose.connect(process.env.CONNECTION_STRING);
-        console.log(`database is connected in :${connect.connection.host}`)
+        await mongoose.connect(process.env.CONNECTION_STRING, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        console.log('Database connected successfully');
     } catch (error) {
-        console.log(error)  
+        console.error('Error connecting to the database:', error.message);
+        process.exit(1);
     }
-}
+};
+
 module.exports = connectDb;
